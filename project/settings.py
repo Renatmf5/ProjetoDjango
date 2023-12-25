aeb91878bfeb474da8cf2cddd153873b18ddc02b
Renato Moreno Fereira <renato_mf5@hotmail.com>
2023-12-25 12:32:48 -0300

    select_related

--- project/settings.py
+++ project/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'recipes',
     'authors',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,3 +152,8 @@ MESSAGE_TAGS = {
     constants.SUCCESS: 'message-success',
     constants.WARNING: 'message-warning',
 }
+
+# Django Debug Toolbar
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
-from recipes.models import Recipe
-from django.http.response import Http404
-from django.db.models import Q
-from utils.pagination import make_pagination
 import os
-from django.views.generic import ListView, DetailView
+from django.db.models import Q
+from django.http import JsonResponse
+from django.http.response import Http404
+from django.views.generic import DetailView, ListView
+from utils.pagination import make_pagination
+from recipes.models import Recipe
+from django.forms.models import model_to_dict
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
@@ -19,6 +20,8 @@ class RecipeListViewBase(ListView):
         qs = qs.filter(
             is_published=True,
         )
+        #select_related é para fazer follow na foreing key usado 1:N ou N:1 ao usar pretech_related é para consultas de muitas para muitos N:N
+        qs = qs.select_related('author', 'category')
         return qs
 
     def get_context_data(self, *args, **kwargs):
@@ -113,4 +116,38 @@ class RecipeDetail(DetailView):
 
         return ctx
 
+class RecipeListViewHomeApi(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
 
+    def render_to_response(self, context, **response_kwargs):
+        recipes = self.get_context_data()['recipes']
+        recipes_list = recipes.object_list.values()
+
+        print(recipes.object_list)
+        return JsonResponse(
+            list(recipes_list),
+            safe=False
+        )
+
+
+class RecipeDetailAPI(RecipeDetail):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+        req = self.request
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.updated_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + \
+                recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+
+        del recipe_dict['is_published']
+        del recipe_dict['preparation_steps_is_html']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False,
+        )
